e90f1d83bf753791e726585ca59de3883fba17a0
Liam Cooper <liam.cooper@andrewjamesworldwide.com>
2018-11-22 15:34:11 +0000

    Updates

---
+++ component---src-pages-page-2-js-5aac582b5113ac0d0eda.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{128:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(132),l=t(139);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-5aac582b5113ac0d0eda.js.map
